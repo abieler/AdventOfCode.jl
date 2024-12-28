c7d9dc4a1e973f9b99105cf4f2ade9c21efab4f3
abieler <andre.bieler81@gmail.com>
2024-12-28 17:09:24 +0100

    Use Tuple as data structure for directions

--- 2024/src/4/4.jl
+++ 2024/src/4/4.jl
@@ -1,30 +1,28 @@
 using BenchmarkTools
 using StaticArrays
 
-function alldirections()
-    return Dict{String,CartesianIndex{2}}(
-        "left" => CartesianIndex(-1, 0),
-        "right" => CartesianIndex(1, 0),
-        "up" => CartesianIndex(0, 1),
-        "down" => CartesianIndex(0, -1),
-        "upleft" => CartesianIndex(-1, 1),
-        "upright" => CartesianIndex(1, 1),
-        "downleft" => CartesianIndex(-1, -1),
-        "downright" => CartesianIndex(1, -1)
-    )
-end
+const directions = (
+    CartesianIndex(-1, 0),
+    CartesianIndex(1, 0),
+    CartesianIndex(0, 1),
+    CartesianIndex(0, -1),
+    CartesianIndex(-1, 1),
+    CartesianIndex(1, 1),
+    CartesianIndex(-1, -1),
+    CartesianIndex(1, -1)
+)
 
 function countxmas(CM, directions)
     N, M = size(CM)
     cnt = 0
-    word = @MArray ['X', '.', '.', '.']
-    xmas = @SArray ['X', 'M', 'A', 'S']
+    word = MVector{4,Char}('X', '.', '.', '.')
+    xmas = SVector{4,Char}('X', 'M', 'A', 'S')
     pos = CartesianIndex(0, 0)
     for idx in CartesianIndices(CM)
         if CM[idx] != 'X'
             continue
         end
-        for direction in values(directions)
+        for direction in directions
             word[2:4] .= '.'
             @inbounds for l = 1:3
                 pos = idx + (l * direction)
@@ -46,6 +44,5 @@ end
 filename = "input.txt"
 data = filename |> readlines .|> l -> reduce(hcat, l)
 charmatrix = vcat(data...)
-directions = alldirections()
 @benchmark countxmas(charmatrix, directions)
 
